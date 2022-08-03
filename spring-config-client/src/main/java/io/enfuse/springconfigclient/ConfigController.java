c636409b4e85b0a8721223e4ad57c8a02b28cd7f
RJ Small <rjsmall@Enfuseios-MBP.askey.com>
2022-08-03 17:39:47 -0400

    get mapping added

--- spring-config-client/src/main/java/io/enfuse/springconfigclient/ConfigController.java
+++ spring-config-client/src/main/java/io/enfuse/springconfigclient/ConfigController.java
@@ -1,11 +1,14 @@
 package io.enfuse.springconfigclient;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RefreshScope
@@ -15,9 +18,18 @@ public class ConfigController {
     @Autowired
     ExternalConfig externalConfig;
 
+    @Autowired
+    StudentRepo studentRepo;
+
 
     @GetMapping("/props")
     public ResponseEntity uploadFileToAzure(){
         return ResponseEntity.ok("property: "+externalConfig.getName());
     }
+
+    @GetMapping("/get_students")
+    public ResponseEntity <List<Student>>getAllStudents() {
+        return ResponseEntity.ok(studentRepo.findAll());
+    }
+
 }

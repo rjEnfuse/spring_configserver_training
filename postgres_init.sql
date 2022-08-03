5042bc185751ff2685cbbcabefadae68736eb1d2
ch-enfuse <chr@enfuse.io>
2022-08-03 09:29:06 -0700

    Added SQL script to seed some data into Postgres.
    Added a SQL script that will execute during boot up of Postgres and inject some
    data into a new database table to use for the client app.

---
+++ postgres_init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE STUDENTS
+(
+    id int PRIMARY KEY,
+    first_name varchar(50)
+);
+
+INSERT INTO students(id, first_name) VALUES
+    (1, 'Angel'),
+    (2, 'Bob'),
+    (3, 'Carl'),
+    (4, 'Jully'),
+    (5, 'Sam'),
+    (6, 'Josh'),
+    (7, 'Will'),
+    (8, 'Fred'),
+    (9, 'Chris'),
+    (10, 'Ryan'),
+    (11, 'Lucy')
